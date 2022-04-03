4240b70765129f7fe7be602896e273c18dd8fcad
Nguyễn Văn Lâm <ngocthau047@gmail.com>
2022-04-03 23:51:32 +0700

    connect database heroku

--- config/db.js
+++ config/db.js
@@ -1,19 +1,34 @@
+// require('dotenv').config();
+// module.exports = {
+//     DB: {
+//         database: process.env.DB_DATABASE_NAME,
+//         username: process.env.DB_USERNAME,
+//         password: process.env.DB_PASSWORD,
+//         CONFIG: {
+//             host: 'my-database-tesla.cn5tyhlgjnd6.us-west-2.rds.amazonaws.com',
+//             ssl: true,
+//             dialect: "postgres",
+//             dialectOptions: {
+//                 ssl: {
+//                     rejectUnauthorized: false
+//                 }
+//             },
+//             logging: console.log,
+//         }
+//     }
+// };
 require('dotenv').config();
 module.exports = {
     DB: {
-        database: process.env.DB_DATABASE_NAME,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
+        URL: process.env.PRODUCTION_DATABASE_URL,
         CONFIG: {
-            host: 'my-database-tesla.cn5tyhlgjnd6.us-west-2.rds.amazonaws.com',
-            ssl: true,
-            dialect: "postgres",
+            logging: true,
             dialectOptions: {
                 ssl: {
+                    require: true,
                     rejectUnauthorized: false
                 }
-            },
-            logging: console.log,
+            }
         }
     }
 };
